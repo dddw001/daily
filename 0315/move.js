1fceb6b9ebfba8433f2e716cb7a0210df35ed85f
dddw001 <2410656679@qq.com>
2018-03-15 22:40:38 +0800

    today

---
+++ 0315/move.js
@@ -0,0 +1,68 @@
+/**
+ * 运动
+ * 多个属性
+ * 有回调函数
+ */
+function getStyle(obj, attr)
+{
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[attr];
+	}
+	else
+	{
+		return getComputedStyle(obj, false)[attr];
+	}
+}
+
+function startMove(obj, json, fn)
+{
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function (){
+		var bStop=true;		//是否到达目标停止运动
+		for(var attr in json)
+		{
+			//1.取当前值
+			var iCur=0;
+			
+			if(attr=='opacity')
+			{
+				iCur=parseInt(parseFloat(getStyle(obj, attr))*100);
+			}
+			else
+			{
+				iCur=parseInt(getStyle(obj, attr));
+			}
+			
+			//2.计算速度
+			var iSpeed=(json[attr]-iCur)/8;
+			iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
+			
+			//3.检测停止
+			if(iCur!=json[attr])
+			{
+				bStop=false;//如果有一个没有达到目标就不停止
+			}
+			
+			if(attr=='opacity')
+			{
+				obj.style.filter='alpha(opacity:'+(iCur+iSpeed)+')';
+				obj.style.opacity=(iCur+iSpeed)/100;
+			}
+			else
+			{
+				obj.style[attr]=iCur+iSpeed+'px';
+			}
+		}
+		
+		if(bStop)
+		{
+			clearInterval(obj.timer);
+			
+			if(fn)
+			{
+				fn();
+			}
+		}
+	}, 30)
+}
